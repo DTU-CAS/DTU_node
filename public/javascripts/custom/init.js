7d78c5589e2157a0ddd3a5c347ef6e0776c2eccd
casperfibaek <casperfibaek@gmail.com>
2016-12-05 09:23:11 +0100

    morning start'

--- public/javascripts/custom/init.js
+++ public/javascripts/custom/init.js
@@ -29,6 +29,23 @@ function init(){
     doubleClickZoom: false,
     editable: true
   });
+  
+  /* 
+     * Workaround for 1px lines appearing in some browsers due to fractional transforms
+     * and resulting anti-aliasing.
+     * https://github.com/Leaflet/Leaflet/issues/3575
+  */
+  (function(){
+      var originalInitTile = L.GridLayer.prototype._initTile
+      L.GridLayer.include({
+          _initTile: function (tile) {
+              originalInitTile.call(this, tile);
+              var tileSize = this.getTileSize();
+              tile.style.width = tileSize.x + 1 + 'px';
+              tile.style.height = tileSize.y + 1 + 'px';
+          }
+      });
+  })()
 
   // GST Ortho 2016
   var GST_Ortho = L.tileLayer.wms('https://kortforsyningen.kms.dk/?servicename=orto_foraar', {
